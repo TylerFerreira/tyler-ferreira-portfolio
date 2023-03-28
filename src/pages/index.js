00f13b893cd277c8766ad883c45f3d985c81105f
Tyler Ferreira <tyferreira4@hotmail.com>
2023-03-28 00:48:40 -0400

    Import Edu font

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1>developedbytyler</h1>
-            <ul>
-              <li><BsFillMoonStarsFill/></li>
-              <li><a href="#">Resume</a></li>
+            <h1 className='text-xl font-roboto font-bold'>developedbytyler</h1>
+            <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+              <li><a href="#" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Resume</a></li>
             </ul>
           </nav>
         </section>
